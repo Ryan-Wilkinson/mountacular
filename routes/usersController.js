5cb83627ec7f1f234ef613a42a20f2b3ea1d82b0
Ryan Wilkinson <ryanw@Ryans-Macbook-Pro.local>
2017-03-25 14:57:47 -0400

    added a user index, though its kinda useless cus it requires authentication to view a users mounts, oh well

--- routes/usersController.js
+++ routes/usersController.js
@@ -4,6 +4,17 @@ var router = express.Router();
 var User = require("../models/userModel.js");
 var Mount = require("../models/mountModel.js");
 
+// USER IDEX, SHOWS ALL USERS SAVED
+router.get('/', function(req, res) {
+  User.find({})
+  .exec(function(err, users){
+    if (err) { console.log(err); }
+    res.render('users/index.hbs', { 
+    	users: users 
+    });
+  });
+});
+
 // USERS NEW CREATE ROUTE
 router.get('/createaccount', function(req, res) {
 	res.render('users/new');
