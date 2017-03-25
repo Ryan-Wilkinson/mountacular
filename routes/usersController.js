d99921ba809bb4512e25c464495f9805ce021b9d
Ryan Wilkinson <ryanw@Ryans-Macbook-Pro.local>
2017-03-25 15:21:37 -0400

    WERE IN BUSINESS, SHITS SAVING TO THE DB NOW BOYS

--- routes/usersController.js
+++ routes/usersController.js
@@ -4,17 +4,6 @@ var router = express.Router();
 var User = require("../models/userModel.js");
 var Mount = require("../models/mountModel.js");
 
-// USER IDEX, SHOWS ALL USERS SAVED
-router.get('/', function(req, res) {
-  User.find({})
-  .exec(function(err, users){
-    if (err) { console.log(err); }
-    res.render('users/index.hbs', { 
-    	users: users 
-    });
-  });
-});
-
 // USERS NEW CREATE ROUTE
 router.get('/createaccount', function(req, res) {
 	res.render('users/new');
@@ -37,6 +26,7 @@ router.post("/", function(req, res) {
 		res.redirect("/sessions/login");
 	});
 });
+
 
 
 
