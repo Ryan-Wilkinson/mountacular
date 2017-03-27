f096c4252d4c5f6d629f44404899005b361161b8
Ryan Wilkinson <ryanw@Ryans-Macbook-Pro.local>
2017-03-27 15:26:39 -0400

    made one seed, gonna take a while to make 10

--- db/seeds.js
+++ db/seeds.js
@@ -20,3 +20,30 @@ mongoose.connection.on('error', function(err) {
 mongoose.connection.once('open', function() {
   console.log("Mongoose has connected to MongoDB!");
 });
+
+// clear before re seed
+Mount.remove({}, function(err) {
+    console.log(err);
+});
+
+var blueBike = new Mount ({
+	name: Blue Bike,
+	spellId: 1,
+	creatureId: 1872364,
+	itemId: 13489257,
+	qualityId: 9,
+	icon: String,     // COME BACK TO THIS ~~!!@$@!$
+	isGround: true,
+	isFlying: false,
+	isAquatic: false,
+	isJumping: true,  // blizzard api ends here
+	dateObtained: Date,
+	foundAt: 'The neighborhood junkyard',
+	obtainedHow: 'Stole that shit',  // found/purchased/stole etc.
+	description: 'A rusty old blue bike that a I found at the junkyard, surprisingly both tires had air. Really fun riding around and jumping curbs'
+});
+
+blueBike.save(function(err) {
+  if (err) console.log(err);
+  console.log('Blue Bike created');
+});
