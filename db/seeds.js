0bdcda9a8d714c7e3da70f2915105b38020a7bab
Ryan Wilkinson <ryanw@Ryans-Macbook-Pro.local>
2017-03-27 16:27:30 -0400

    made a giant snake seed

--- db/seeds.js
+++ db/seeds.js
@@ -37,12 +37,166 @@ var blueBike = new Mount ({
 	isFlying: false,
 	isAquatic: false,
 	isJumping: true,  // blizzard api ends here
-	foundAt: 'The neighborhood junkyard',
-	obtainedHow: 'Stole that shit',
+	foundAt: 'The neighborhood junkyard.',
+	obtainedHow: 'Stole that shit.',
 	description: 'A rusty old blue bike that a I found at the junkyard, surprisingly both tires had air. Really fun riding around and jumping curbs'
 });
+
+var giantCock = new Mount ({
+	name: 'Giant Chicken',
+	spellId: 2,
+	creatureId: 1872365,
+	itemId: 13489258,
+	qualityId: 100,
+	icon: 'http://i.imgur.com/q3n6YVs.jpg',
+	isGround: true,
+	isFlying: true,
+	isAquatic: false,
+	isJumping: true,  // blizzard api ends here
+	foundAt: 'On the internet.',
+	obtainedHow: 'Pooped it out.',
+	description: 'Giant robust, colorful, and regal cock that I can ride around on and even fly for a short duration.'
+});
+
+var giantSnake = new Mount ({
+	name: 'Giant Snake',
+	spellId: 3,
+	creatureId: 1872365,
+	itemId: 13489258,
+	qualityId: 65,
+	icon: 'http://vignette4.wikia.nocookie.net/cryptidz/images/2/2c/Giant_snake.jpg/revision/latest?cb=20141014163913',
+	isGround: true,
+	isFlying: false,
+	isAquatic: false,
+	isJumping: false,  // blizzard api ends here
+	foundAt: 'The Amazon Jungle Boi.',
+	obtainedHow: 'Tamed his ass.',
+	description: 'A giant snake I tamed while exploring in the amazon, can ride around and scare the hell out of people.'
+});
+
+// var blueBike = new Mount ({
+// 	name: 'Blue Bike',
+// 	spellId: 1,
+// 	creatureId: 1872364,
+// 	itemId: 13489257,
+// 	qualityId: 9,
+// 	icon: 'http://www.bcbsnc.com/assets/campaigns/public/gonc/images/9234-bike.jpg',
+// 	isGround: true,
+// 	isFlying: false,
+// 	isAquatic: false,
+// 	isJumping: true,  // blizzard api ends here
+// 	foundAt: 'The neighborhood junkyard',
+// 	obtainedHow: 'Stole that shit',
+// 	description: 'A rusty old blue bike that a I found at the junkyard, surprisingly both tires had air. Really fun riding around and jumping curbs'
+// });
+
+// var blueBike = new Mount ({
+// 	name: 'Blue Bike',
+// 	spellId: 1,
+// 	creatureId: 1872364,
+// 	itemId: 13489257,
+// 	qualityId: 9,
+// 	icon: 'http://www.bcbsnc.com/assets/campaigns/public/gonc/images/9234-bike.jpg',
+// 	isGround: true,
+// 	isFlying: false,
+// 	isAquatic: false,
+// 	isJumping: true,  // blizzard api ends here
+// 	foundAt: 'The neighborhood junkyard',
+// 	obtainedHow: 'Stole that shit',
+// 	description: 'A rusty old blue bike that a I found at the junkyard, surprisingly both tires had air. Really fun riding around and jumping curbs'
+// });
+
+// var blueBike = new Mount ({
+// 	name: 'Blue Bike',
+// 	spellId: 1,
+// 	creatureId: 1872364,
+// 	itemId: 13489257,
+// 	qualityId: 9,
+// 	icon: 'http://www.bcbsnc.com/assets/campaigns/public/gonc/images/9234-bike.jpg',
+// 	isGround: true,
+// 	isFlying: false,
+// 	isAquatic: false,
+// 	isJumping: true,  // blizzard api ends here
+// 	foundAt: 'The neighborhood junkyard',
+// 	obtainedHow: 'Stole that shit',
+// 	description: 'A rusty old blue bike that a I found at the junkyard, surprisingly both tires had air. Really fun riding around and jumping curbs'
+// });
+
+// var blueBike = new Mount ({
+// 	name: 'Blue Bike',
+// 	spellId: 1,
+// 	creatureId: 1872364,
+// 	itemId: 13489257,
+// 	qualityId: 9,
+// 	icon: 'http://www.bcbsnc.com/assets/campaigns/public/gonc/images/9234-bike.jpg',
+// 	isGround: true,
+// 	isFlying: false,
+// 	isAquatic: false,
+// 	isJumping: true,  // blizzard api ends here
+// 	foundAt: 'The neighborhood junkyard',
+// 	obtainedHow: 'Stole that shit',
+// 	description: 'A rusty old blue bike that a I found at the junkyard, surprisingly both tires had air. Really fun riding around and jumping curbs'
+// });
+
+// var blueBike = new Mount ({
+// 	name: 'Blue Bike',
+// 	spellId: 1,
+// 	creatureId: 1872364,
+// 	itemId: 13489257,
+// 	qualityId: 9,
+// 	icon: 'http://www.bcbsnc.com/assets/campaigns/public/gonc/images/9234-bike.jpg',
+// 	isGround: true,
+// 	isFlying: false,
+// 	isAquatic: false,
+// 	isJumping: true,  // blizzard api ends here
+// 	foundAt: 'The neighborhood junkyard',
+// 	obtainedHow: 'Stole that shit',
+// 	description: 'A rusty old blue bike that a I found at the junkyard, surprisingly both tires had air. Really fun riding around and jumping curbs'
+// });
+
+// var blueBike = new Mount ({
+// 	name: 'Blue Bike',
+// 	spellId: 1,
+// 	creatureId: 1872364,
+// 	itemId: 13489257,
+// 	qualityId: 9,
+// 	icon: 'http://www.bcbsnc.com/assets/campaigns/public/gonc/images/9234-bike.jpg',
+// 	isGround: true,
+// 	isFlying: false,
+// 	isAquatic: false,
+// 	isJumping: true,  // blizzard api ends here
+// 	foundAt: 'The neighborhood junkyard',
+// 	obtainedHow: 'Stole that shit',
+// 	description: 'A rusty old blue bike that a I found at the junkyard, surprisingly both tires had air. Really fun riding around and jumping curbs'
+// });
+
+// var blueBike = new Mount ({
+// 	name: 'Blue Bike',
+// 	spellId: 1,
+// 	creatureId: 1872364,
+// 	itemId: 13489257,
+// 	qualityId: 9,
+// 	icon: 'http://www.bcbsnc.com/assets/campaigns/public/gonc/images/9234-bike.jpg',
+// 	isGround: true,
+// 	isFlying: false,
+// 	isAquatic: false,
+// 	isJumping: true,  // blizzard api ends here
+// 	foundAt: 'The neighborhood junkyard',
+// 	obtainedHow: 'Stole that shit',
+// 	description: 'A rusty old blue bike that a I found at the junkyard, surprisingly both tires had air. Really fun riding around and jumping curbs'
+// });
 
 blueBike.save(function(err) {
   if (err) console.log(err);
   console.log('Blue Bike created');
+});
+
+giantSnake.save(function(err) {
+  if (err) console.log(err);
+  console.log('Giant Snake created');
+});
+
+giantCock.save(function(err) {
+  if (err) console.log(err);
+  console.log('Giant Cock created');
 });
