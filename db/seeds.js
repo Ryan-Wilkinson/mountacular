141f401cf678899aa733d8be8f763ee64a791b10
Ryan Wilkinson <ryanw@Ryans-Macbook-Pro.local>
2017-03-27 16:31:21 -0400

    t rex seed created

--- db/seeds.js
+++ db/seeds.js
@@ -37,8 +37,8 @@ var blueBike = new Mount ({
 	isFlying: false,
 	isAquatic: false,
 	isJumping: true,  // blizzard api ends here
-	foundAt: 'The neighborhood junkyard.',
-	obtainedHow: 'Stole that shit.',
+	foundAt: 'The neighborhood junkyard',
+	obtainedHow: 'Stole that shit',
 	description: 'A rusty old blue bike that a I found at the junkyard, surprisingly both tires had air. Really fun riding around and jumping curbs'
 });
 
@@ -53,42 +53,42 @@ var giantCock = new Mount ({
 	isFlying: true,
 	isAquatic: false,
 	isJumping: true,  // blizzard api ends here
-	foundAt: 'On the internet.',
-	obtainedHow: 'Pooped it out.',
+	foundAt: 'On the internet',
+	obtainedHow: 'Pooped it out',
 	description: 'Giant robust, colorful, and regal cock that I can ride around on and even fly for a short duration.'
 });
 
 var giantSnake = new Mount ({
 	name: 'Giant Snake',
 	spellId: 3,
-	creatureId: 1872365,
-	itemId: 13489258,
+	creatureId: 1872366,
+	itemId: 13489259,
 	qualityId: 65,
 	icon: 'http://vignette4.wikia.nocookie.net/cryptidz/images/2/2c/Giant_snake.jpg/revision/latest?cb=20141014163913',
 	isGround: true,
 	isFlying: false,
 	isAquatic: false,
 	isJumping: false,  // blizzard api ends here
-	foundAt: 'The Amazon Jungle Boi.',
-	obtainedHow: 'Tamed his ass.',
+	foundAt: 'The Amazon Jungle Boi',
+	obtainedHow: 'Tamed his ass',
 	description: 'A giant snake I tamed while exploring in the amazon, can ride around and scare the hell out of people.'
 });
 
-// var blueBike = new Mount ({
-// 	name: 'Blue Bike',
-// 	spellId: 1,
-// 	creatureId: 1872364,
-// 	itemId: 13489257,
-// 	qualityId: 9,
-// 	icon: 'http://www.bcbsnc.com/assets/campaigns/public/gonc/images/9234-bike.jpg',
-// 	isGround: true,
-// 	isFlying: false,
-// 	isAquatic: false,
-// 	isJumping: true,  // blizzard api ends here
-// 	foundAt: 'The neighborhood junkyard',
-// 	obtainedHow: 'Stole that shit',
-// 	description: 'A rusty old blue bike that a I found at the junkyard, surprisingly both tires had air. Really fun riding around and jumping curbs'
-// });
+var tRex = new Mount ({
+	name: 'T-Rex',
+	spellId: 4,
+	creatureId: 187237,
+	itemId: 13489260,
+	qualityId: 1000,
+	icon: 'https://s-media-cache-ak0.pinimg.com/564x/3c/79/e0/3c79e0c936fee27b3f158e11034d132d.jpg',
+	isGround: true,
+	isFlying: false,
+	isAquatic: false,
+	isJumping: true,  // blizzard api ends here
+	foundAt: 'The Museum',
+	obtainedHow: 'Family Heirloom',
+	description: 'A motha fuckin t rex, holy shit.'
+});
 
 // var blueBike = new Mount ({
 // 	name: 'Blue Bike',
@@ -189,6 +189,11 @@ var giantSnake = new Mount ({
 blueBike.save(function(err) {
   if (err) console.log(err);
   console.log('Blue Bike created');
+});
+
+tRex.save(function(err) {
+  if (err) console.log(err);
+  console.log('T-Rex created');
 });
 
 giantSnake.save(function(err) {
