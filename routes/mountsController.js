047814c9866c8d7468569beba9c99f5b78714e8d
Ryan Wilkinson <ryanw@Ryans-Macbook-Pro.local>
2017-03-27 17:45:40 -0400

    can successfully load edit mount page

--- routes/mountsController.js
+++ routes/mountsController.js
@@ -1,11 +1,24 @@
-var express = require("express");
-router = express.Router({ mergeParams: true });
+var express = require('express');
+router = express.Router({mergeParams: true});
 var User = require("../models/userModel.js");
 var Mount = require("../models/mountModel.js");
+var authHelpers = require('../helpers/authHelpers.js');
 
-//HOME PAGE
-router.get('/', function(req, res, next) {
-  res.render('index');
+// GET EDIT MOUNT
+router.get('/:id/edit', function(req, res) {
+	User.findById(req.params.userId)
+       .exec(function(err, user) {
+         if (err) { console.log("not authorized"); }
+
+	Mount.findById(req.params.id)
+		.exec(function(err, mount) {
+			if (err) { console.log(err); }
+			res.render('mounts/edit', {
+				mount: mount,
+				user: user
+			});
+		});
+	});
 });
 
 
