ce6ac9ddec1cae8d6d79fc44b3967cba64383a9b
Ryan Wilkinson <ryanw@Ryans-Macbook-Pro.local>
2017-04-07 15:05:22 -0400

    dot env stuff

--- app.js
+++ app.js
@@ -9,14 +9,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var methodOverride = require('method-override');
+require('dotenv').config();
 
 // Connect to database
-if (process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI);
-}
-else {
-  mongoose.connect('mongodb://localhost/mountacular');
-}
+mongoose.connect(process.env.MONGODB_URI);
+
 mongoose.connection.on('error', function(err) {
   console.error('MongoDB connection error: ' + err);
   process.exit(-1);
